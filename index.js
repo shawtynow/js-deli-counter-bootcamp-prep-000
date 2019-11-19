d8ae2fd249d5ba568f4ace3ac18336c85ff7c6f9
comical-tag-7436 <shawtynow2008@gmail.com>
2019-11-19 03:02:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,16 @@ function nowServing(line) {
   return `Currently serving ${line.shift()}.`
 }
 
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = []
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+}
